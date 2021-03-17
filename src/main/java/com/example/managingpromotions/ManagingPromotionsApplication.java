501e0c0116e6412bb73ebe3c4c5b46793ac8698a
Krisu1007 <krystek10071@wp.pl>
2021-03-17 16:04:25 +0100

    Initial commit

---
+++ src/main/java/com/example/managingpromotions/ManagingPromotionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.managingpromotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagingPromotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagingPromotionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/managingpromotions/ManagingPromotionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managingpromotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagingPromotionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
