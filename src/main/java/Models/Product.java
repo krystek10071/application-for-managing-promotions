4460ee642406603a75f9415eb9e40fe9cb0ebfbc
Krisu1007 <krystek10071@wp.pl>
2021-03-31 12:52:48 +0200

    Add Product Entity

---
+++ src/main/java/Models/Product.java
@@ -0,0 +1,27 @@
+package Models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity(name = "PRODUCT")
+public class Product{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected long id;
+
+    @Column(name="PRODUCT_NAME", length = 50, nullable = false)
+    private String productName;
+
+    @Column(name="DESCRIPTION")
+    private String description;
+
+    @Column(name="PRICE", nullable = false)
+    private double price;
+
+    @Column(name="CATEGORY")
+    private String category;
+
+    @Column(name="EXPIRY_DATE")
+    private Date expiryDate;
+}
